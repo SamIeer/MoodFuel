3b241075931572daa41911e547cca65592aaa5e9
Sameer Chauhan <sameerchauhan212204@gmail.com>
2025-12-16 23:17:39 +0530

    pydantic model

---
+++ app/schema.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+class MoodInput(BaseModel):
+    sleep_hours: float
+    stress_level: int
+    time_of_day: int 
+    workload_level: int
+
+'''
+This ensures clean, validated input data -- no missing or invalid fields when someone hits your API
+'''
